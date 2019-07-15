f72376f26a95503944a28a00f7f82a50860194dc
Sherpa Dee <dfoettinger@gmail.com>
2019-07-15 20:09:29 +0200

    Vortrag Korrektur + getcwd

--- browsertest.py
+++ browsertest.py
@@ -1,20 +1,23 @@
 from selenium import webdriver
 import time
+import os
 
+
+cwd = os.getcwd()
 ##edge (windows only)
-#browser = webdriver.Edge(executable_path = './msedgedriver')
+#browser = webdriver.Edge(executable_path = cwd +'/msedgedriver')
 #browser.get('https://www.google.com') 
 #time.sleep(5)
 #browser.quit()
 
 ##firefox
-browser = webdriver.Firefox(executable_path = 'geckodriver')
+browser = webdriver.Firefox(executable_path = cwd+ '/geckodriver')
 browser.get('https://www.google.com') 
 time.sleep(5)
 browser.quit()
 
 ##chrome
-browser = webdriver.Chrome(executable_path = 'chromedriver')
+browser = webdriver.Chrome(executable_path = cwd + '/chromedriver')
 browser.get('https://www.google.com') 
 time.sleep(5)
 browser.quit()
