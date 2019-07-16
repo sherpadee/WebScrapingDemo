bea077fcf49e238a2d0d127e163d565390f0e3b1
sherpadee <df@roeschke.net>
2019-07-16 17:25:11 +0200

    Twitter

--- twitter.py
+++ twitter.py
@@ -1,6 +1,7 @@
 # Import the Twython class
 from twython import Twython  
 import json
+from tabulate import tabulate
 
 # Load credentials from json file
 with open("twitter_credentials-df.json", "r") as file:  
@@ -12,21 +13,21 @@ python_tweets = Twython(creds['consumer_key'], creds['consumer_secret'])
 # Create our query
 query = {'q': '#nuedigital',  
         'result_type': 'newest',
-        'count': 30,
+        'count': 20,
         'lang': 'de',
         }
 
 import pandas as pd
 
 # Search tweets
-dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}  
+dict_ = {'user': [],  'text': [], 'fav_count': []}  
 for status in python_tweets.search(**query)['statuses']:  
     dict_['user'].append(status['user']['screen_name'])
-    dict_['date'].append(status['created_at'])
+    #dict_['date'].append(status['created_at'])
     dict_['text'].append(status['text'])
-    dict_['favorite_count'].append(status['favorite_count'])
+    dict_['fav_count'].append(status['favorite_count'])
 
 # Structure data in a pandas DataFrame for easier manipulation
 df = pd.DataFrame(dict_)  
-df.sort_values(by='favorite_count', inplace=True, ascending=False)  
-print(df.head()  )
+df.sort_values(by='fav_count', inplace=True, ascending=False)  
+print(tabulate(df, headers='keys', tablefmt='grid'))
